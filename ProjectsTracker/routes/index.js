0393fe659a2c6ba0020d42b65b157ae3f08eef85
KaloyanIT <kaloyanit@yahoo.com>
2016-11-19 22:01:35 +0200

    Add eslint and eslint config Remove eslint errors

--- ProjectsTracker/routes/index.js
+++ ProjectsTracker/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//  next parameter on router.get function
+router.get("/", (req, res) => {
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
