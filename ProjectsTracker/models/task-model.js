a7e8443229817991ae29c36c800302455b73c346
Barrelrolla <chetkara@gmail.com>
2016-11-21 12:01:25 +0200

    Added a few things in the Task model

--- ProjectsTracker/models/task-model.js
+++ ProjectsTracker/models/task-model.js
@@ -4,3 +4,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // TODO:
+const Statuses = ["Open", "Closed", "Resolved", "Reopened", "Waiting For", "Duplicate"];
+
+const TaskSchema = new mongoose.Schema({
+    Name: { type: String, required: true, min: 3, max: 50 },
+    Description: { type: String, max: 500 },
+    Priority: { type: Number, required: true, min: 1, max: 10 },
+    CreatedDate: { type: Date, default: Date.now },
+    UpdatedDate: { type: Date },
+    DueDate: {
+        type: Date,
+        required: true,
+        validate: function (v) {
+            return v > this.CreatedDate;
+        }
+    },
+    ReporterID: { type: Number, required: true },
+    AssigneeID: { type: Number, required: true },
+    Status: { type: String, required: true, enum: Statuses },
+    ProjectID: { type: Number, required: true },
+    Comments: {
+        Title: String,
+        Content: String,
+        isDeleted: Boolean,
+        PosterID: Number
+    }
+});
