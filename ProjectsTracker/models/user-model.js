c166795099eebe5d6dd0a4c2d2a53aea29f320a5
Alex Nestorov <nestorov.alexander@gmail.com>
2016-11-21 18:52:48 +0200

    add user-model

--- ProjectsTracker/models/user-model.js
+++ ProjectsTracker/models/user-model.js
@@ -1,6 +1,55 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// TODO:
+let UserSchema = new Schema({
+  FirstName: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: true
+  },
+  Username: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    },
+    validate: {
+      validator: function(v) {
+        return /[A-Za-z0-9]{6}/.test(v);
+      },
+      message: '{VALUE} is not a valid username!'
+    },
+  },
+  Password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /[A-Za-z0-9]{8}/.test(v);
+      },
+      message: '{VALUE} is not a valid password!'
+    },
+  },
+  Role: {
+    type: Number
+  },
+  ProjectWorkingOnId: {
+    type: Number
+  },
+  BugWorkingOnId: {
+    type: Number
+  }
+});
+
+UserSchema
+.virtual('FullName')
+.get(function () {
+  return this.FirstName + ' ' + this.LastName;
+});
+
+module.exports = mongoose.model("User", UserSchema);
