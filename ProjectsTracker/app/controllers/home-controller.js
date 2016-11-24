9b4649554c3e633e4bf582715de79076bfcac313
Barrelrolla <chetkara@gmail.com>
2016-11-24 19:29:46 +0200

    Added navbar in the pug

--- ProjectsTracker/app/controllers/home-controller.js
+++ ProjectsTracker/app/controllers/home-controller.js
@@ -4,9 +4,9 @@ const data = require("../data")({ Project });
 
 module.exports = {
     index(req, res) {
-        //// TODO: parse json - pug integration 
-        data.getAllProjects().then(projects => res.json(projects))
-        .then(jsonProjects => res.render("../views/index.pug", { title: "JSONPLACE" }));                  
-        //res.render("../views/index.pug", );
+        // TODO: parse json - pug integration
+        // data.getAllProjects().then(projects => res.json(projects))
+        // .then(jsonProjects => res.render("../views/index.pug", { title: "JSONPLACE" }));
+        res.render("../views/index.pug", { title: "Bugs" });
     }
 };
