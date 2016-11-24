f32ae03215ec9fb8467adc41559daf9d37d83ebf
KaloyanIT <kaloyanit@yahoo.com>
2016-11-24 20:57:56 +0200

    test changes

--- ProjectsTracker/app/controllers/home-controller.js
+++ ProjectsTracker/app/controllers/home-controller.js
@@ -5,8 +5,8 @@ const data = require("../data")({ Project });
 module.exports = {
     index(req, res) {
         //// TODO: parse json - pug integration 
-        data.getAllProjects().then(projects => res.json(projects))
-        .then(jsonProjects => res.render("../views/index.pug", { title: "JSONPLACE" }));                  
-        //res.render("../views/index.pug", );
+        //data.getAllProjects().then(projects => res.json(projects))
+        //.then(jsonProjects => res.render("../views/index.pug", { title: "JSONPLACE" }));                  
+        res.render("../views/index.pug", { title : "Pesho" } );
     }
 };
