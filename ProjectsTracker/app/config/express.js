a40221f166004613e984e8d7e19f6eeaed23c73b
J0hnyBG <ivan.parvanov1@gmail.com>
2016-11-27 13:21:12 +0200

    moved configuration to express config

--- ProjectsTracker/app/config/express.js
+++ ProjectsTracker/app/config/express.js
@@ -1,6 +1,5 @@
 /* globals require */
 
-
 let express = require("express"),
     session = require("express-session"),
     bodyParser = require("body-parser"),
@@ -13,11 +12,8 @@ let express = require("express"),
 
 let path = require("path");
 
-
 module.exports = function(app, config) {
 
-    //require("../routers")(app);
-    //app.use(bodyParser());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(cookieParser());
@@ -48,15 +44,34 @@ module.exports = function(app, config) {
       res.locals.error = req.flash("error");
       next();
     });
-    //app.use(passport.initialize());
-    //app.use(passport.session());
 
-    // app.use((req, res, next) => {
-    //     //console.log(res);
-    //     // console.log(res);
-    //     // console.log(next);
-    //     error = new Error("Not Found");
-    //     error.status = 404;
-    //     next(error);
-    // });
+    const User = require("../models/user-model");
+    const Task = require("../models/task-model");
+    const Project = require("../models/project-model");
+    const data = require("../data")({ User, Task, Project });
+    const controllers = require("../controllers")(data);
+
+    require("../routers")(app, controllers);
+    require("../config/mongoose")(config);
+
+    app.use((req, res, next) => {
+        let err = new Error("We can't seem to find the page you are looking for!");
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    app.use((err, req, res, next) => {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        if (req.app.get("env") === "development") {
+            res.locals.error = err;
+        }
+        else {
+            res.locals.error = {};
+        }
+        // render the error page
+        res.status(err.status || 500);
+        res.render("error");
+    });
 };
--- ProjectsTracker/server.js
+++ ProjectsTracker/server.js
@@ -8,15 +8,5 @@ let config = require("./app/config/config")[env];
 
 require("./app/config/express")(app, config);
 
-const User = require("./app/models/user-model");
-const Task = require("./app/models/task-model");
-const Project = require("./app/models/project-model");
-const data = require("./app/data")({ User, Task, Project });
-const controllers = require("./app/controllers")(data);
-
-require("./app/routers")(app, controllers);
-require("./app/config/mongoose")(config);
-require("./app/config/express-error")(app);
-
 app.listen(config.port);
 console.log(`Server running on http://localhost:${config.port}`);
