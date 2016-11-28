8116e2bfe455a16e9ce80edb014c2a74f048ba92
Alex Nestorov <nestorov.alexander@gmail.com>
2016-11-28 21:53:21 +0200

    fix duplicates

--- ProjectsTracker/app/routers/user-router.js
+++ ProjectsTracker/app/routers/user-router.js
@@ -5,14 +5,13 @@ const user = require("../config/roles");
 module.exports = function (server, userController) {
     server.get("/users", userController.viewAllUsers);
     server.get("/user/:name", userController.viewUserByName);
-
     server.get("/register", userController.registerPage);
     server.post("/register", userController.createUser);
     server.get("/profile", userController.getProfile);
 
     server.post("/login", userController.loginLocal);
-    server.get("/login", userController.login);
 
+    server.get("/login", userController.login);
     server.get("/logout", userController.logout);
 
     server.get("/admin", user.can("access admin page"), userController.admin);
