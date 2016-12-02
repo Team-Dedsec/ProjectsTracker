d3dad5e98a6a68a80a3b5fb284b79790a3b8e14f
KaloyanIT <kaloyanit@yahoo.com>
2016-12-02 23:59:41 +0200

    remove old files

--- ProjectsTracker/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/mongodb/index.d.ts" />
-/// <reference path="globals/mongoose/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/debug/index.d.ts" />
