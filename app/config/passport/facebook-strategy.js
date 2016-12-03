44dac65fa466937be1c3520fba5a48c1a295e70a
Alex Nestorov <nestorov.alexander@gmail.com>
2016-12-03 10:15:09 +0200

    fix facebook url

--- app/config/passport/facebook-strategy.js
+++ app/config/passport/facebook-strategy.js
@@ -10,7 +10,7 @@ module.exports = function(passport, data) {
   passport.use(new FacebookStrategy({
       clientID: FACEBOOK_APP_ID,
       clientSecret: FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3001/auth/facebook/return",
+      callbackURL: "https://dedsec.herokuapp.com/auth/facebook/return",
       profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'displayName','timezone', 'updated_time', 'verified']
     },
     function(accessToken, refreshToken, profile, done) {
